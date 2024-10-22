f7771ac4c2fd710ba1336837fbe5ed8577857c75
Siddharth Sharma <vsssiddharth@gmail.com>
2024-10-22 22:22:05 +0530

    default api route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,5 +21,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/invoices", invoiceRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
